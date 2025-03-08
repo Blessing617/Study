10f2dc12a5d1b6844bb59505fe69c437000d36fd
Blessing <535104678@qq.com>
2025-03-08 20:29:30 +0800

    测试

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-学习
+用户：Blessing
+1.leetcode 刷题个人使用
+2.git 仓库管理学习
--- test.c
+++ test.c
@@ -4,41 +4,8 @@
 #include <math.h>
 #include <ctype.h>
 
-int rotateString(char *s, char *goal)
-{
-    int len = strlen(s);
-    char top;
-    int cnt = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-        top = s[0];
-        for (int j = 0; j < len - 1; j++)
-        {
-            s[j] = s[j + 1];
-        }
-        s[len - 1] = top;
-
-        printf("%s\r\n", s);
-
-        for (int n = 0; n < len; n++)
-        {
-            if (s[n] != goal[n])
-                break;
-            else
-                cnt++;
-            if (cnt == len)
-                return 1;
-        }
-    }
-    return 0;
-}
-
 int main(void)
 {
-    char s[] = "abcde";
-    char goal[] = "cdeab";
-    rotateString(s, goal);
-
+    printf("Hello!\r\n");
     return 0;
 }
