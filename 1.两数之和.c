5563dbb490858343ab705b8f08e14e8e91dcc140
Blessing617 <535104678@qq.com>
2025-03-17 15:27:38 +0800

    finish

--- 1.两数之和.c
+++ 1.两数之和.c
@@ -10,5 +10,21 @@
  */
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
+    for ( int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i]+nums[j]==target)
+            {
+                int* result = malloc(sizeof(int)*2);
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }        
+    *returnSize = 0;
+    return NULL;
 }
 // @lc code=end
